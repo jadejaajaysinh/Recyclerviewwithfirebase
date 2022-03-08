b0fd31d1bd95564b50fce69a5364358c4958a12f
jadejaajaysinh <86345632+jadejaajaysinh@users.noreply.github.com>
2022-03-08 15:40:28 +0530

    first commit

---
+++ app/src/main/java/com/example/realtimerecyclerview/User.java
@@ -0,0 +1,19 @@
+package com.example.realtimerecyclerview;
+
+public class User {
+
+    String firstName, lastName, age;
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
